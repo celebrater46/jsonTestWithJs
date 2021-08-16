c305e0b1551bcd123818681457659e140ff784ae
hideru <jota.16.stigma.world@gmail.com>
2021-08-16 17:51:19 +0900

    the codes has been moved

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+// https://teratail.com/questions/245824
+let el = document.getElementById("output");
+const getjson = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                el.innerText = xhr.responseText;
+            } else {
+                alert("status = " + status.status);
+            }
+        }
+    }
+    xhr.open("GET", "json/test.json");
+    xhr.send();
+}
+
+const testJson = () => {
+    getjson();
+}
